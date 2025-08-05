1053c4f1aa9c2641ee8c5140a857525c37502a60
Prasad <prasadzoman15@gmail.com>
2025-08-06 01:18:00 +0530

    Update Navbar.jsx

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-brand">Dashboard</div>
       <ul className="navbar-links">
-        <li><a href="#">Overview</a></li>
+        <li><a href="" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>Overview</a></li>
         <li><a href="#charts">Charts</a></li>
         <li><a href="#metrics">Metrics</a></li>
         <li><a href="#campaign">Campaigns</a></li>
